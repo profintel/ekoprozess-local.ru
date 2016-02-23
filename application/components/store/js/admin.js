3b3e82a17eb4aac09a910e5b76504e983cac8f6f
lenok <lenok.git@gmail.com>
2016-02-23 23:14:21 +0500

    убрала автоматическое изменение остатков при изменении брутто, потому что потом можно запутаться какой реальный остаток, если будет несколько неотправленных в остатки объектов

--- application/components/store/js/admin.js
+++ application/components/store/js/admin.js
@@ -19,15 +19,11 @@ function sendComingMovement(path){
 * Подсчет остатков сырья на складе
 */
 function updateRestProduct(obj){
-  var form, section, active, store_type_id, client_id, products, form_block, rest;
+  var form, store_type_id, client_id, products, form_block, rest;
   form =  $(obj).parents('form');
   // смотрим client_id и пересчитываем все остатки по указанному вторсырью
   store_type_id = form.find('input[name="store_type_id"]').val();
   client_id = form.find('select[name="client_id"]').val();
-  // Тип формы (приход или расход)
-  section = form.find('input[name="section"]').val();
-  // Наличие объекта в остатках
-  active = form.find('input[name="active"]').val();
   if(store_type_id && client_id){
     // находим все select-ы с вторсырьем и запрашиваем остатки
     products = form.find('select[name="product_id[]"]');
@@ -43,32 +39,6 @@ function updateRestProduct(obj){
             if(result){
               form_block.find('.rest').text(result.rest);
               form_block.find('.rest_all').text(result.rest_all);
-              
-              // если тип склада первичная продукция и приход не отправлен в остатки, добавляем брутто к остаткам
-              if(active == 0 && store_type_id == 1 && section == 'coming'){
-                rest = form_block.find('input[name="gross[]"]').val();
-                form_block.find('.rest').text((result.rest - 0) + (rest - 0));
-                form_block.find('.rest_all').text((result.rest_all - 0) + (rest - 0));
-              }
-              // если тип склада первичная продукция и расход не отправлен в остатки, вычитаем брутто из остатков
-              if(active == 0 && store_type_id == 1 && section == 'expenditure'){
-                rest = form_block.find('input[name="gross[]"]').val();
-                form_block.find('.rest').text(result.rest - rest);
-                form_block.find('.rest_all').text(result.rest_all - rest);
-              }
-
-              // если тип склада готовая продукция и приход не отправлен в остатки, добавляем брутто к остаткам
-              if(active == 0 && store_type_id == 2 && section == 'coming'){
-                rest = form_block.find('input[name="net[]"]').val();
-                form_block.find('.rest').text((result.rest - 0) + (rest - 0));
-                form_block.find('.rest_all').text((result.rest_all - 0) + (rest - 0));
-              }
-              // если тип склада готовая продукция и расход не отправлен в остатки, вычитаем брутто из остатков
-              if(active == 0 && store_type_id == 2 && section == 'expenditure'){
-                rest = form_block.find('input[name="net[]"]').val();
-                form_block.find('.rest').text(result.rest - rest);
-                form_block.find('.rest_all').text(result.rest_all - rest);
-              }
             }
           },
         'json')
