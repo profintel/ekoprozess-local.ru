5125f150710821642adeeb1ab400a1ead74728b9
lenok <lenok.git@gmail.com>
2016-05-05 15:33:01 +0500

    удалила компонент Карта сайта

--- application/components/sitemap/js/site.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  if ($('input[name="query_string"]').length) {
-    $('input[name="query_string"]').click(function(){
-      if ($(this).val() == "Поиск...") {
-        $(this).val("");
-      }
-    });
-  }
-})
