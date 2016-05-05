77abcad84b6c3a44819126652bae6f2993ba8483
lenok <lenok.git@gmail.com>
2016-05-05 15:29:48 +0500

    удалила компонент Каталог

--- application/components/catalog/templates/catalog_categories.php
+++
@@ -1,5 +0,0 @@
-<? foreach ($categories as $category) { ?>
-    <div class="catalog_menu_item<? if ($_page['path'] == $category['path']) { ?> selected_category<? } ?>">
-        <a class="catalog_category" href="<?=$category['path']?>"><?=$category['title']?></a>
-    </div>
-<? } ?>
