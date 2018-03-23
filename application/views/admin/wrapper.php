e2b75983a2cf09c8ee481d325b4195916ccece2b
elena <elena@profintel.ru>
2018-03-23 15:18:40 +0500

    вернула ckeditor на все страницы

--- application/views/admin/wrapper.php
+++ application/views/admin/wrapper.php
@@ -11,9 +11,7 @@
   <link rel="stylesheet" type="text/css" href="/components/tp_admin.css?<?=time();?>" />
   
   <script type="text/javascript" src="/components/tp_admin.js?<?=time();?>"></script>
-  <? if(in_array('edit_client', $this->segments)) {?>
-    <script type="text/javascript" src="/adm/ckeditor/ckeditor.js"></script>
-  <?}?>
+  <script type="text/javascript" src="/adm/ckeditor/ckeditor.js"></script>
   <script type="text/javascript" src="/adm/chosen/chosen.jquery.min.js"></script> 
 
   <!--[if gte IE 9]>
